a5b1d9806818ee6a109cbbd04b2377abb0589a34
pranavkumar <pranavkumar.s2022eee@sece.ac.in>
2025-07-14 19:29:21 +0530

    ui_with_backend

--- static/js/landing.js
+++ static/js/landing.js
@@ -385,3 +385,11 @@ function setStatusDot(roomId, status) {
         }
     }
 }
+
+
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    const mainContent = document.querySelector('.main-content');
+    sidebar.classList.toggle('open');
+    mainContent.classList.toggle('shift');
+}
